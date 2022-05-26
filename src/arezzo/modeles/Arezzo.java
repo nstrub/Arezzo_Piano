77ffc71f182903de786999097951c8f9d327466f
strub17u <nicolas.strub1@etu.univ-lorraine.fr>
2022-05-26 16:36:34 +0200

    Fonction assezDePlace qui regarde si il y a la place et ajoute la note si c'est le cas A faire : apparition de la partition

--- src/arezzo/modeles/Arezzo.java
+++ src/arezzo/modeles/Arezzo.java
@@ -12,7 +12,7 @@ public class Arezzo {
     private Partition parti;
     private Synthesizer synthe;
     private StringBuilder notes;
-    private int nbNotes;
+    private double nbNotes;
     private String forme;
     private String octave;
 
@@ -31,8 +31,8 @@ public class Arezzo {
         parti = new Partition(synthe);
         notes = new StringBuilder();
         nbNotes = 0;
-        forme = "noire";
-        octave = "medium";
+        forme = "Noire";
+        octave = "Medium";
     }
 
     public Partition getPartition(){
@@ -55,42 +55,81 @@ public class Arezzo {
         String noteAdd;
         /////On gère le nombre de note/////
         if(note.equals("chut")){
-            for(int i = nbNotes; i < 4; i++){
+            for(double i = nbNotes; i < 4; i++){
                 this.notes.append("-");
             }
             this.notes.append("|");
             this.nbNotes = 0;
         }
         else{
-            this.nbNotes ++;
-            if(nbNotes == 5){
-                this.notes.append("|");
-                this.nbNotes = 1;
-            }
-            /////On quelle note on ajoute/////
-                //En premier on regarde l'octave//
-            if(this.getOctave().equals("Grave")){
-                noteAdd = note + ",";
-            } else if (this.getOctave().equals("Aigu")) {
-                noteAdd = note.toLowerCase();
-            }
-            else{   //Note en medium
-                noteAdd = note;
-            }
+            if(assezDePlace(this.getForme())){
+                if(this.getOctave().equals("Grave")){
+                    noteAdd = note + ",";
+                } else if (this.getOctave().equals("Aigu")) {
+                    noteAdd = note.toLowerCase();
+                }
+                else{   //Note en medium
+                    noteAdd = note;
+                }
                 //Puis on ajoute la durée corespondante//
-            if(this.getForme().equals("Croche")){
-                noteAdd = noteAdd + "/";
-            } else if (this.getForme().equals("Blanche")) {
-                noteAdd = noteAdd + "2";
-            } else if (this.getForme().equals("Ronde")) {
-                noteAdd = noteAdd + "4";
-            } //Si forme == noire, on ne change rien
-            System.out.println(this.getOctave() + " " +this.getForme());
-            this.notes.append(noteAdd);
-            parti.setMelodie(noteAdd);
-            parti.play(noteAdd);
+                if(this.getForme().equals("Croche")){
+                    noteAdd = noteAdd + "/";
+                } else if (this.getForme().equals("Blanche")) {
+                    noteAdd = noteAdd + "2";
+                } else if (this.getForme().equals("Ronde")) {
+                    noteAdd = noteAdd + "4";
+                } //Si forme == noire, on ne change rien
+                System.out.println(this.getOctave() + " " +this.getForme());
+                this.notes.append(noteAdd);
+                if(this.nbNotes == 4){
+                    this.notes.append("|");
+                    this.nbNotes = 0;
+                }
+                parti.setMelodie(noteAdd);
+                parti.play(noteAdd);
+            }
+            else {
+                System.out.println("Pas assez de place frère");
+                for(double i = nbNotes; i < 4; i++){
+                    this.notes.append("-");
+                }
+                this.notes.append("|");
+                this.nbNotes = 0;
+            }
         }
         System.out.println("Voila ta mélo pour l'instant bg (stringBuilder) " + notes);
+
+    }
+
+    /**
+     * Augmente nbNotes en fonction de la durée de la note voulue et regarde si on peut la posée
+     * @param forme
+     * @return
+     */
+    public boolean assezDePlace(String forme){
+        if(forme.equals("Noire")){    // + 1
+            this.nbNotes ++;
+            if (this.nbNotes > 4) {
+                return false;
+            }
+        }
+        if(forme.equals("Croche")){   // + 0.5
+            this.nbNotes = nbNotes + 0.5;
+            if (this.nbNotes > 4) {
+                return false;
+            }
+        } else if (forme.equals("Blanche")) { // + 2
+            this.nbNotes += 2;
+            if (this.nbNotes > 4) {
+                return false;
+            }
+        } else if (forme.equals("Ronde")) {   // + 4
+            this.nbNotes += 4;
+            if (this.nbNotes > 4) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void changerOctave(String newOctave){
